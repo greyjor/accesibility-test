14f913e5b7a3b61b140d6b3a84812f29ae125c3c
greyjor <grejordal@gmail.com>
2022-10-21 19:58:15 +0200

    Add new tests to the spec accessibility_tests.cy.js

--- cypress/e2e/my_practices/accessibility_tests.cy.js
+++ cypress/e2e/my_practices/accessibility_tests.cy.js
@@ -2,16 +2,41 @@
 
 describe('Validate accessibility TodoMVC application', () => {
   
-    
-
-  it('should log any accessibility failures', () => {
-
+  beforeEach(() => {
     cy.visit('https://todomvc.com/examples/react/#/');
     // Cypress-Axe library
     cy.injectAxe();
+  });
+    
+
+  it('should log any accessibility failures', () => {
     // command cy.checkA11y or cy.checkAccessibility, scan your page for any accessibility failures
     cy.checkA11y();
   })
+
+  it('should exclude specific elements on the page', () => {
+    // exclude elements of a page from being tested
+    cy.checkA11y({ exclude: ['.source-links > :nth-child(1)'] });  
+  });
+  
+  it('should only test specific element on the page', () => {
+    // indicates only the element that you want to test
+    cy.checkA11y('.source-links > :nth-child(1)');
+  });
+
+  it('should only include rules with serious impacts', () => {
+    //Includes only the elements for which the impact is serious
+    cy.checkA11y(null, { includedImpacts: ['serious'] });
+  });
+  
+  it('should exclude specific accessibility rules', () => {
+    //exclude accessibility rule "region"
+    cy.checkA11y(null, {
+      rules: {
+        'region': { enabled: false },
+      },
+    });
+  });
 
   
 })
